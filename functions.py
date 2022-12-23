f2a150b336c226982c3a3050309e1ea3a3560eed
ibrhmies <02220201044@ogr.inonu.edu.tr>
2022-12-23 22:27:02 +0300

    functions
    fonksiyonlar

---
+++ functions.py
@@ -0,0 +1,11 @@
+def ibo():
+    for i in range(11):
+        print("ibo")
+
+#bu şekilde ibo diye bir fonksiyon tanımlarsak programı çalıştırdığımızda fonksiyon çalışmayacak 
+#çünkü fonksiyonu tanımlamadık
+ibo()
+#bu şekilde fonksiyonun adını yazıp yanına parantez koyarak 
+#fonksiyonu çağırmış oluyoruz ve ekrana ibo yazdırıyor
+#fonkksiyonun içine for döngüsü ekleyerek fonksiyonu 
+#bir kere çağırdığımızda ekrana 10 kere ibo yazdırabiliriz
